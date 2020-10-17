87f171a21af1136c198cd544fa172315d3bd56c3
yanwanpeng <921896405@qq.com>
2020-10-17 14:21:50 +0800

    路由拦截

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
         <ul class="headertop">
-            <li>1</li>
-            <li>1</li>
+            <li class="header-logo"></li>
+            <li class="header-mid">
+                <span :class="{active: hIndex == index}" v-for="(item, index) in headerList" :key="index" @click="hIndex = index">{{item}}</span>
+            </li>
             <li>
+                <span class="usericon"><i class="el-icon-s-custom"></i></span>
                 <span>
                     超级管理员
                 </span>
-                <el-dropdown>
+                <el-dropdown @command="setUser">
                     <span class="el-dropdown-link setting-header">
                         <i class="el-icon-setting"></i>
                     </span>
-                    <el-dropdown-menu slot="dropdown" @command="setUser">
-                        <el-dropdown-item v-for="(item, index) in dropdownList" :key="index">
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item :command="item" v-for="(item, index) in dropdownList" :key="index">
                             <i :class="iconList[index]"></i>
                             {{item}}
                         </el-dropdown-item>
@@ -27,13 +30,24 @@
 export default {
     data() {
         return {
+            hIndex: 0,
             dropdownList: ['个人信息', '修改信息', '退出'],
             iconList: ['el-icon-user', 'el-icon-edit-outline', 'el-icon-switch-button'],
+            headerList:['首页', '合同管理']
         }
     },
     methods:{
+        signout(){
+            this.$router.replace({path: '/'});
+            // userItem
+        },
         setUser(item) {
-            console.log(item, 'item')
+            switch (item) {
+                case '个人信息':break;
+                case '修改信息':break;
+                case '退出': this.signout();break;
+                default:break;
+            }
         }
     }
 }
